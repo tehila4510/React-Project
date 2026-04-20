2c8df2d47b506779f4fff56f1809999833781b77
Tehila ST <tehila4510@gmail.com>
2026-04-20 23:33:43 +0300

    add store

--- my-react-app/src/app/store.jsx
+++ my-react-app/src/app/store.jsx
@@ -1,19 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userApi } from "../features/User/Redux/api";
-import {userSliceReducer} from "../features/User/Redux/userSlice"
+import userReducer from "../features/User/Redux/userSlice";
 import { userAnswerApi } from "../features/UserAnswer/Redux/api";
 import { questionApi } from "../features/Question/Redux/api";
+import { skillApi } from "../features/Skill/Redux/api";
+import { userSkillProgressApi } from "../features/UserSkillProgress/Redux/api";
+import { questionOptionApi } from "../features/Option/Redux/api";
+import { sessionApi } from "../features/Session/Redux/api";
+import { quizApi } from "../features/Quiz/Redux/api";
 
 export const store= configureStore({
     reducer: {
-        userSlice: userSliceReducer,
-        [userApi.userPath]: userApi.reducer,
-
-        //להוסיף SLICE
-        [userAnswerApi.userPath]: userAnswerApi.reducer,
-        [questionApi.reducerPath]: questionApi.reducer
-
+        
+        [userApi.reducerPath]: userApi.reducer,        
+        [userAnswerApi.reducerPath]: userAnswerApi.reducer,
+        [userSkillProgressApi.reducerPath]: userSkillProgressApi.reducer,
+        [questionApi.reducerPath]: questionApi.reducer,
+        [questionOptionApi.reducerPath]:questionOptionApi.reducer,
+        [sessionApi.reducerPath]:sessionApi.reducer,
+        [skillApi.reducerPath]: skillApi.reducer,
+        [quizApi.reducerPath]:  quizApi.reducer,
+        user: userReducer
     },
-     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware),
+      middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(userApi.middleware)
+      .concat(userAnswerApi.middleware)
+      .concat(userSkillProgressApi.middleware)
+      .concat(questionApi.middleware)
+      .concat(questionOptionApi.middleware)
+      .concat(sessionApi.middleware)
+      .concat(skillApi.middleware)
+      .concat(quizApi.middleware)
+,
 });
