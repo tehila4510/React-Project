753a8936dba3b6576536d014421b0a1d211cd484
Tehila ST <tehila4510@gmail.com>
2026-04-20 23:38:47 +0300

    fixing

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,13 +1,31 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import GlottieApp from './app/GlottieApp.jsx'
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { useSelector } from 'react-redux';
+import AuthPage   from './auth/AuthPage.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider>
-      
-      <App />
+export function Root() {
+  const { isLoggedIn, currentUser } = useSelector((state) => state.user);
+
+  if (!isLoggedIn) {
+    return <AuthPage />;
+  }
+
+  if (!currentUser?.currentLevel) {
+    return <AuthPage />;
+  }
+
+  return <GlottieApp />;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Root />
     </Provider>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
